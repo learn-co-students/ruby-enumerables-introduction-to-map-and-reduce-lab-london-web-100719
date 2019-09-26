165d57a26d25271d82341a16138481475750595a
Faris Aziz <farisaziz12@gmail.com>
2019-09-26 21:42:35 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,27 @@
-map_to_negativize([1, 2, 3, -9]) {
-  (1..4 + 9).map {|i| i * -1}
-}
+def map_to_negativize(source_array)
+  source_array.map { |i| i * -1 }
+end
+
+def map_to_no_change(source_array)
+  source_array
+end
+
+def map_to_double(source_array)
+  source_array.map { |i| i * 2 }
+end
+
+def map_to_square(source_array)
+  source_array.map { |i| i * i }
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  source_array.reduce(starting_point) { |total, x| total + x }
+end
+
+def reduce_to_all_true(source_array)
+  source_array.reduce(true) {|result, i| result && i }
+end
+
+def reduce_to_any_true(source_array)
+  source_array.reduce(false) {|result, i| result || i || false}
+end
